e12479917709e71ff73bf38eacefd73ffbbcf079
Jamie Barton <hi_jamie@me.com>
2017-10-09 13:09:53 +0100

    Updated confirmEmail function

--- src/sendOrderConfirmationEmail.js
+++ src/sendOrderConfirmationEmail.js
@@ -1,39 +1,39 @@
 'use latest'
 
 const sgMail = require('@sendgrid/mail')
-const { fromEvent } = require('graphcool-lib')
 
-module.exports = event =>
-  new Promise((resolve, reject) => {
-    if (!process.env.SENDGRID_API_KEY) {
-      console.log('Please provide a valid SENDGRID_API_KEY')
-      resolve({ error: 'CommerceQL is not configured to work with SendGrid.' })
-    }
+module.exports = event => {
+  if (!process.env['SENDGRID_API_KEY']) {
+    console.log('Please provide a valid SENDGRID_API_KEY')
+    return { error: 'CommerceQL is not configured to work with SendGrid.' }
+  }
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  sgMail.setApiKey(process.env['SENDGRID_API_KEY'])
 
-    const { node } = event.data.Order
+  const { node } = event.data.Order
 
-    const message = {
-      to: node.email,
-      from: `${process.env.STORE_NAME} <${process.env.STORE_EMAIL}>`,
-      subject: `We've got your order!`,
-      text: `Hi ${node.billingName}. Your order has been received!`,
-      html: `
-        <p>${node.billingName}</p>
-        <p>Just to let you know, your order has been received!</p>
-      `
-    }
+  const message = {
+    to: node.email,
+    from: `${process.env['STORE_NAME']} <${process.env['STORE_EMAIL']}>`,
+    subject: `We've got your order!`,
+    text: `Hi ${node.billingName}. Your order has been received!`,
+    html: `
+      <p>Hi ${node.billingName}</p>
+      <p>Your order has been received!</p>
+    `
+  }
 
+  return new Promise((resolve, reject) => {
     sgMail
       .send(message)
       .then(() => {
-        return { data: { success: true } }
+        return resolve()
       })
       .catch(error => {
         console.log('Email could not be sent because an error occured:')
         console.log(error)
 
-        return { data: { success: false } }
+        return resolve({ error: error.message })
       })
-  })()
+  })
+}
