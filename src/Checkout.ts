b4c1415999027642b2bfa57e49ff86e0e3a34a53
Clayton Ray <iamclaytonray@gmail.com>
2017-11-22 04:46:39 -0500

    Add support for TS & Prettier

--- src/Checkout.ts
+++ src/Checkout.ts
@@ -1,14 +1,12 @@
-'use latest'
-
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-const {fromEvent} = require('graphcool-lib')
+import { fromEvent } from 'graphcool-lib';
+import stripe from 'stripe';
 
 module.exports = event =>
   new Promise((resolve, reject) => {
-    let {basketId} = event.data
+    const { basketId } = event.data;
 
-    const graphcool = fromEvent(event)
-    const api = graphcool.api('simple/v1')
+    const graphcool = fromEvent(event);
+    const api = graphcool.api('simple/v1');
 
     const getBasket = basketId => {
       const query = `query getBasketById($basketId: ID!) {
@@ -25,46 +23,46 @@ module.exports = event =>
             }
           }
         }
-      }`
+      }`;
 
       const variables = {
-        basketId
-      }
+        basketId,
+      };
 
-      return api.request(query, variables)
-    }
+      return api.request(query, variables);
+    };
 
     const getOrCreateStripeCustomer = (user, stripeToken) => {
       if (user.stripeCustomerId) {
-        return Promise.resolve(user.stripeCustomerId)
+        return Promise.resolve(user.stripeCustomerId);
       }
 
-      const {email, firstName, lastName} = user
+      const { email, firstName, lastName } = user;
 
       return new Promise((resolve, reject) => {
         stripe.customers.create(
           {
             email,
             description: `${firstName} ${lastName}`,
-            source: stripeToken
+            source: stripeToken,
           },
           (err, customer) => {
             if (err) {
-              reject(err)
+              reject(err);
             } else {
-              resolve(customer.id)
+              resolve(customer.id);
             }
-          }
-        )
-      })
-    }
+          },
+        );
+      });
+    };
 
     // clean up
     // sanitize
     // pass in data object as arg instead of relying on event.data
     const chargeStripeCustomer = (amount, description, stripeCustomerId) =>
       new Promise((resolve, reject) => {
-        let charge = {
+        const charge = {
           amount,
           description,
           currency: event.data.currency || 'gbp',
@@ -77,31 +75,31 @@ module.exports = event =>
               line1: event.data.shippingLine1,
               line2: event.data.shippingLine2,
               postal_code: event.data.shippingPostalCode,
-              state: event.data.shippingState
+              state: event.data.shippingState,
             },
-            name: `${event.data.firstName} ${event.data.lastName}`
+            name: `${event.data.firstName} ${event.data.lastName}`,
           },
           metadata: {
-            shipping_instructions: event.data.shippingInstructions
-          }
-        }
+            shipping_instructions: event.data.shippingInstructions,
+          },
+        };
 
         if (event.data.phone) {
-          let charge = Object.assign({}, charge, {
-            receipt_phone: event.data.phone
-          })
+          const charge = Object.assign({}, charge, {
+            receipt_phone: event.data.phone,
+          });
         }
 
         return stripe.charges.create(charge, (err, charge) => {
           if (err) {
             // handle declined transaction
             // update Basket as payment failed
-            reject(err)
+            reject(err);
           } else {
-            resolve(stripeCustomerId)
+            resolve(stripeCustomerId);
           }
-        })
-      })
+        });
+      });
 
     const convertBasketToOrder = variables => {
       const mutation = `mutation createOrder(
@@ -167,67 +165,74 @@ module.exports = event =>
           shippingInstructions
           orderTotal
         }
-      }`
+      }`;
 
-      return api.request(mutation, variables)
-    }
+      return api.request(mutation, variables);
+    };
 
     return getBasket(basketId)
-      .then(({Basket}) => {
+      .then(({ Basket }) => {
         if (!Basket) {
-          throw new Error(`Invalid basketId ${basketId}`)
+          throw new Error(`Invalid basketId ${basketId}`);
         }
 
-        const {items} = Basket
+        const { items } = Basket;
 
-        const {data} = event
-        const {stripeToken, firstName, lastName, email, stripeCustomerId} = data
+        const { data } = event;
+        const {
+          stripeToken,
+          firstName,
+          lastName,
+          email,
+          stripeCustomerId,
+        } = data;
 
         // cleanup
         const user = !!stripeCustomerId
-          ? {firstName, lastName, email}
-          : {firstName, lastName, email, stripeCustomerId}
+          ? { firstName, lastName, email }
+          : { firstName, lastName, email, stripeCustomerId };
 
         // cleanup
-        const orderTotal = getOrderTotal(items)
-        const description = getDescription(items)
+        const orderTotal = getOrderTotal(items);
+        const description = getDescription(items);
 
         // Convert to async/await
         return getOrCreateStripeCustomer(user, stripeToken)
           .then(stripeCustomerId =>
-            chargeStripeCustomer(orderTotal, description, stripeCustomerId)
+            chargeStripeCustomer(orderTotal, description, stripeCustomerId),
           )
           .then(stripeCustomerId =>
             convertBasketToOrder(
               Object.assign({}, data, {
                 stripeCustomerId,
-                orderTotal
-              })
-            )
+                orderTotal,
+              }),
+            ),
           )
-          .then(({createOrder}) => {
+          .then(({ createOrder }) => {
             return resolve(
               Object.assign(
                 {},
                 {
-                  data: createOrder
+                  data: createOrder,
                 },
-                user
-              )
-            )
-          })
+                user,
+              ),
+            );
+          });
       })
-      .catch(error => resolve({error: error.message}))
-  })
+      .catch(error => resolve({ error: error.message }));
+  });
 
 const getOrderTotal = items =>
-  items.reduce((sum, item) => sum + item.orderedItem.amount * item.quantity, 0)
+  items.reduce((sum, item) => sum + item.orderedItem.amount * item.quantity, 0);
 
 const getDescription = items =>
   items
     .map(
       item =>
-        `${item.quantity}x ${item.orderedItem.name} @ $${item.orderedItem
-          .amount} each`
+        `${item.quantity}x ${item.orderedItem.name} @ $${
+          item.orderedItem.amount
+        } each`,
     )
-    .join(', ')
+    .join(', ');
