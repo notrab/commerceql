0ed424055ff01e50aff1d5ee78f1e07c69fba5ce
Jamie Barton <hi_jamie@me.com>
2017-11-24 20:35:40 +0000

    Removed example hello query

--- src/hello.ts
+++
@@ -1,9 +0,0 @@
-export default async event => {
-  await new Promise(r => setTimeout(r, 50));
-
-  return {
-    data: {
-            message: `Hello ${event.data.name || 'World'}`,
-    },
-  };
-};
